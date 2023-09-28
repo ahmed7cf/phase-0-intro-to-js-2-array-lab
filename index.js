38132fa3f05a3f451f70b32c903728ec410cb2e1
Ahmed Ibrahim Ahmed <ahmed7cf@gmail.com>
2023-09-28 13:04:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(newCat) {
+    cats.push(newCat);
+}
+
+function destructivelyPrependCat(newCat) {
+    cats.unshift(newCat);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(cat) {
+    return [...cats, cat];
+}
+
+function prependCat(cat) {
+    return [cat, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1, cats.length)
+}
